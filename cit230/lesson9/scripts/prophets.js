86756e76e1d004bf8f114c941ce95e8d7f800e05
cdandrews96 <50116548+cdandrews96@users.noreply.github.com>
2019-11-14 09:51:23 -0700

    prophets before style

--- cit230/lesson9/scripts/prophets.js
+++ cit230/lesson9/scripts/prophets.js
@@ -13,8 +13,64 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
         response.json()
             .then(
                 response => {
-                    let prophet = response.prophets[0];
+                    const prophets = response.prophets;
 
-                    console.log(prophet.name);
-                });
+                    prophets.forEach(
+                        (prophet) => {
+                            //DEBUG
+                            console.log(prophet);
+
+                            let article = document.createElement('article');
+                            let h2 = document.createElement('h2');
+
+                            h2.textContent = prophet.name + ' ' + prophet.lastname;
+
+                            article.appendChild(h2);
+
+                            let birthdateDiv = document.createElement('div');
+
+                            //Birth Date
+
+                            let birthdateLabel = document.createElement('label');
+                            birthdateLabel.textContent = 'Date of Birth: ';
+
+                            let birthdateSpan = document.createElement('span');
+                            birthdateSpan.textContent = prophet.birthdate;
+
+
+
+                            article.appendChild(birthdateLabel);
+                            article.appendChild(birthdateSpan);
+
+                            article.appendChild(birthdateDiv);
+
+                            //Birth Place
+                            let birthplaceDiv = document.createElement('div');
+
+                            let birthplaceLabel = document.createElement('label');
+                            birthplaceLabel.textContent = 'Place of Birth: ';
+
+                            let birthplaceSpan = document.createElement('span');
+                            birthplaceSpan.textContent = prophet.birthplace;
+
+                            article.appendChild(birthplaceLabel);
+                            article.appendChild(birthplaceSpan);
+
+                            //Add Image
+                            let imageDiv = document.createElement('div');
+                            let image = document.createElement('img');
+
+                            image.setAttribute('src', prophet.imageurl);
+                            image.setAttribute('alt', prophet.name + ' ' + prophet.lastname + ' - ' + prophet.order);
+
+                            imageDiv.appendChild(image);
+
+                            article.appendChild(imageDiv);
+
+
+                            document.querySelector('section.prophets').appendChild(article);
+                        }
+                    );
+
+                    });
     });
