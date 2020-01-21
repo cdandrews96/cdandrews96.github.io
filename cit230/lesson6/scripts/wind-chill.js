3099ebd89d05eae9dcc2decd31970ef167967932
cdandrews96 <50116548+cdandrews96@users.noreply.github.com>
2020-01-20 17:58:58 -0700

    change

--- cit230/lesson6/scripts/wind-chill.js
+++
@@ -1,7 +0,0 @@
-let currentTemp = 41; // t
-let windSpeed = 8; // s
-let windChill = Math.floor(35.74 + 0.6215*currentTemp - 35.75*Math.pow (windSpeed, 0.16) + 0.4275*currentTemp*Math.pow(windSpeed, 0.16));
-
-document.getElementById("currentTemp").innerHTML = currentTemp + '&deg;';
-document.getElementById("windSpeed").innerHTML = windSpeed + 'mph';
-document.getElementById("windChill").innerHTML = Math.round(windChill) + '&deg;';
