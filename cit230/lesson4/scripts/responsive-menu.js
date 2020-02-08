58822c2a80e011c0ba554057bd8d96a59298e2de
cdandrews96 <50116548+cdandrews96@users.noreply.github.com>
2020-02-07 17:46:44 -0700

    preston home page start

---
+++ cit230/lesson4/scripts/responsive-menu.js
@@ -0,0 +1,11 @@
+// const hambutton = document.querySelector(".ham");
+// hambutton.addEventListener("click", toggleMenu, false);
+
+// function toggleMenu() {
+
+//     document.querySelector(".navigation").classList.toggle("responsive");
+// }
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
