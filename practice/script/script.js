3b88b134660c14f19fc562d251500d11bb1664ee
cdandrews96 <and16137@byui.edu>
2020-05-19 02:25:41 -0600

    new page

---
+++ practice/script/script.js
@@ -0,0 +1,13 @@
+// BUTTON PRACTICE
+
+// document.getElementById('btn').addEventListener('click', show);
+
+// function show() {
+//     alert('Hello World!')
+// }
+
+
+// const button = document.querySelector('#btn');
+
+// button.addEventListener('click', () => {button.classList.add('newstyle')});
+
---
+++ wdd230/lesson4/script/currentDate.js
@@ -0,0 +1,5 @@
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+console.log(currentYear);
+document.getElementById("currentYear").innerHTML = currentYear;
+
