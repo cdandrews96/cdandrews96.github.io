5cebf78e3f51699c1f107585427f66c3da41b042
cdandrews96 <and16137@byui.edu>
2021-08-17 09:34:21 -0600

    final project

---
+++ wdd230/lesson11/script/weatherTodayFishHaven.js
@@ -0,0 +1,13 @@
+const apiTodayURL = "//api.openweathermap.org/data/2.5/weather?id=5585000&units=imperial&appid=cec0709cb1fd453037d20ba93d946a93"
+
+fetch(apiTodayURL)
+    .then((response) => response.json())
+    .then((object) => {
+        console.log(object);
+
+    document.getElementById('currentWeather').textContent = object.weather[0].main;
+    document.getElementById('tempHigh').textContent = object.main.temp_max;
+    document.getElementById('humidity').textContent = object.main.humidity;
+    document.getElementById('windSpeed').textContent = object.wind.speed;
+
+    });
